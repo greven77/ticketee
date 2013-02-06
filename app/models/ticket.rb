2c07080e62c294e512d6ecd6b2c5b21688b93b8a
Greven77 <greven77@gmail.com>
2013-02-06 13:20:55 +0000

    Added basic email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -11,6 +11,8 @@ class Ticket < ActiveRecord::Base
 
   has_many :comments
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",
+    :class_name => "User"
 
   attr_accessible :description, :title, :assets_attributes, :tag_names
   attr_accessor :tag_names
@@ -18,6 +20,7 @@ class Ticket < ActiveRecord::Base
   validates :description, :presence => true, :length => { :minimum => 10 }
 
   before_create :associate_tags
+  after_create :creator_watches_me
 
   private
     def associate_tags
@@ -25,6 +28,12 @@ class Ticket < ActiveRecord::Base
         tag_names.split(" ").each do |name|
           self.tags << Tag.find_or_create_by_name(name)
         end
+      end
+    end
+
+    def creator_watches_me
+      if user
+        self.watchers << user unless self.watchers.include?(user)
       end
     end
 
