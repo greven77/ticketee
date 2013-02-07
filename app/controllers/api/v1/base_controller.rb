d07ba7d38d11177c3ecf92578ce4f303fb48a47f
Greven77 <greven77@gmail.com>
2013-02-07 00:37:35 +0000

    Add rate limiting to the V1 API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::BaseController < ActionController::Base
 	respond_to :json, :xml
 
 	before_filter :authenticate_user
+	before_filter :check_rate_limit
 
 	private
 		def authenticate_user
@@ -13,6 +14,15 @@ class Api::V1::BaseController < ActionController::Base
 
 		def current_user
 			@current_user
+		end
+
+		def check_rate_limit
+			if @current_user.request_count > 100
+				error = { :error => "Rate limit exceeded." }
+				respond_with(error, :status => 403)
+			else
+				@current_user.increment!(:request_count)
+			end
 		end
 
 		def authorize_admin!
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206192558) do
+ActiveRecord::Schema.define(:version => 20130206234845) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(:version => 20130206192558) do
     t.string   "unconfirmed_email"
     t.boolean  "admin",                  :default => false
     t.string   "authentication_token"
+    t.integer  "request_count",          :default => 0
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
